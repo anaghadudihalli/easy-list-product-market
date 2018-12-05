ad3fb50de30467117de5a1f63c996ccd3ad73656
Anagha Dudihalli <Anagha.Dudihalli@betsol.com>
2018-12-05 14:00:44 +0530

    Adding a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return _MyAppState();
+    }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +23,9 @@ class MyApp extends StatelessWidget {
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+
+                  },
                   child: Text('Add Product'),
                 ),
               ),

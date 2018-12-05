eeee3d90f6af3ebc2af6dcf9d3fb50bcd2ecd02b
Anagha Dudihalli <Anagha.Dudihalli@betsol.com>
2018-12-05 14:21:07 +0530

    Adding a list

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return _MyAppState();
-    }
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,19 +25,21 @@ class _MyAppState extends State<MyApp> {
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {
-
-                  },
+                  onPressed: () {},
                   child: Text('Add Product'),
                 ),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('Food Paradise')
-                  ],
-                ),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
               ),
             ],
           )),

dafeefdc9020704b008b109e42aedc52ead71c28
Anagha Dudihalli <Anagha.Dudihalli@betsol.com>
2018-12-05 14:34:19 +0530

    Adding function to button

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,12 @@ class _MyAppState extends State<MyApp> {
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                    _products.add('Advanced Food Tester');
+                    print(_products);                      
+                    });
+                  },
                   child: Text('Add Product'),
                 ),
               ),

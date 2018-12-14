6fc3f7694f411f08ab06c6172bc6582fafff7d68
Anagha Dudihalli <anaghadudihalli@gmail.com>
2018-12-14 13:36:25 +0530

    Adding alert dialog and modal

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -5,36 +5,65 @@ class ProductPage extends StatelessWidget {
   final String title;
   final String imageUrl;
   ProductPage(this.title, this.imageUrl);
+
+  _showWarningDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action cannot be undone'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Discard'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('Continue'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
         onWillPop: () {
           print('Back button pressed');
           Navigator.pop(context, false);
-          return Future.value(false); // making value true here tries to pop page twice. ie. tries to pop
+          return Future.value(
+              false); // making value true here tries to pop page twice. ie. tries to pop
           // root page too. As a result, the app crashes. keep it false.
         },
         child: Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text('Details!'),
+          appBar: AppBar(
+            title: Text(title),
           ),
-          Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true),
-                color: Theme.of(context).accentColor,
-              )),
-        ],
-      ),
-    ));
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Details!'),
+              ),
+              Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    child: Text('DELETE'),
+                    onPressed: ()=>
+                      _showWarningDialog(context),
+                    color: Theme.of(context).accentColor,
+                  )),
+            ],
+          ),
+        ));
   }
 }
--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 class ProductCreatePage extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return Center(child: Text('Create Product'));
-    }
+  Widget build(BuildContext context) {
+    return Center(
+        child: RaisedButton(
+      child: Text('Save'),
+      onPressed: () {
+        showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Center(child: Text('This is a modal'));
+            });
+      },
+    ));
+  }
 }
